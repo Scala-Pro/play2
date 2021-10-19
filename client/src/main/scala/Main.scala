ba23baff7e1dbfd8e453eca679354eb3ba94ca70
Prince <prince777_98@mail.ru>
2021-10-19 20:10:32 +0500

    initial commit in reactjs

--- client/src/main/scala/Main.scala
+++ client/src/main/scala/Main.scala
@@ -1,3 +1,22 @@
+import japgolly.scalajs.react.ScalaComponent
+import japgolly.scalajs.react.component.Scala.BackendScope
+import japgolly.scalajs.react.vdom.html_<^._
+import org.scalajs.dom.document
+import org.scalajs.dom.html.Div
+
 object Main extends App {
-  println("hello world!")
+ case class State()
+
+  class Backend($: BackendScope[Unit, State]) {
+    def render(implicit state: State): VdomTagOf[Div] =
+      <.div("Hello reactJs")
+  }
+
+  val AppComponent =
+    ScalaComponent.builder[Unit]
+      .initialState(State())
+      .renderBackend[Backend]
+      .build
+
+  AppComponent().renderIntoDOM(document.getElementById("app"))
 }
