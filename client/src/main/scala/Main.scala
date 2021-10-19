f1441d0ea190c1a9f3abafc38a96102a1989224c
Prince <prince777_98@mail.ru>
2021-10-19 20:55:17 +0500

    create form

--- client/src/main/scala/Main.scala
+++ client/src/main/scala/Main.scala
@@ -1,15 +1,35 @@
-import japgolly.scalajs.react.ScalaComponent
+import japgolly.scalajs.react.{Callback, ScalaComponent}
+import japgolly.scalajs.react.callback.CallbackTo
 import japgolly.scalajs.react.component.Scala.BackendScope
+import japgolly.scalajs.react.facade.SyntheticFormEvent
 import japgolly.scalajs.react.vdom.html_<^._
-import org.scalajs.dom.document
+import org.scalajs.dom.{console, document}
 import org.scalajs.dom.html.Div
+import org.scalajs.dom.raw.HTMLInputElement
 
 object Main extends App {
- case class State()
+ case class State(login: String = "")
 
   class Backend($: BackendScope[Unit, State]) {
+
+    def onChangeLogin(e: SyntheticFormEvent[HTMLInputElement]): Callback =
+      $.modState(_.copy(login = e.target.value))
+
+    def loginForm(implicit state: State): VdomTagOf[Div] =
+      <.div(^.cls := "row")(
+        <.div(^.cls := "col-6 offset-3")(
+          <.div(^.cls := "form-group") (
+            <.label(^.cls := "form-label")("Login:"),
+            <.input(^.cls := "form-control", ^.onChange ==> onChangeLogin)
+          )
+        ),
+        <.div(^.cls := "col-6 offset-3")(
+          "Length of data: " + state.login.length
+        )
+      )
+
     def render(implicit state: State): VdomTagOf[Div] =
-      <.div("Hello reactJs")
+      <.div(^.cls := "container")(loginForm)
   }
 
   val AppComponent =
