a8edf9871eab73ba4f5f63836526eff712bd1d80
Rasulbek <rasul_k1985@bk.ru>
2021-10-31 01:05:18 +0500

    game den

--- client/src/main/scala/Game.scala
+++ client/src/main/scala/Game.scala
@@ -27,11 +27,45 @@ class Game extends AjaxImplicits {
     def playButton(implicit state: State): VdomTagOf[Button] =
       <.button(^.cls := "btn btn-primary btn-md", ^.onClick --> getAllPrize)("Get Free Prize")
 
-    def gameRoulette(implicit state: State): TagMod =
-      ???
-
+    def gameRoulette(implicit state: State): VdomTagOf[Div] = {
+      <.div(
+        <.div(^.className := "wraper",
+          <.div(^.className := "arrowup"),
+          <.div(^.className := "arrowdown"),
+          <.div(^.className := "window",
+            <.ul(^.className := "list"),
+            <.ul(^.className := "list",
+              <.li(
+                <.img(^.src := "https://cdn0.iconfinder.com/data/icons/fruits/128/Strawberry.png", ^.alt := "")
+              ),
+              <.li(
+                <.img(^.src := "https://cdn0.iconfinder.com/data/icons/fruits/128/Cherry.png", ^.alt := "")
+              ),
+              <.li(
+                <.img(^.src := "https://cdn0.iconfinder.com/data/icons/fruits/128/Apple.png", ^.alt := "")
+              ),
+              <.li(
+                <.img(^.src := "https://cdn0.iconfinder.com/data/icons/fruits/128/Lemon.png", ^.alt := "")
+              ),
+              <.li(
+                <.img(^.src := "https://cdn0.iconfinder.com/data/icons/fruits/128/Kiwi.png", ^.alt := "")
+              ),
+              <.li(
+                <.img(^.src := "https://cdn0.iconfinder.com/data/icons/fruits/128/Pear.png", ^.alt := "")
+              )
+            )
+          )
+        ),
+        <.p(^.className := "text-center",
+          <.button(^.className := "button", "Кнопка")
+        ),
+        <.div(^.className := "win",
+          <.ul
+        )
+      )
+    }
     def render(implicit state: State): VdomTagOf[Div] =
-      <.div(playButton)
+      <.div(gameRoulette, playButton)
   }
 
   val AppComponent: AppComponentType =
